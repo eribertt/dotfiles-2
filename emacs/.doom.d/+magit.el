f87d0d73441f6d3381ad362eff0cbdc02995ae14
Aria Edmonds <aria@ar1as.space>
2018-06-05 13:43:32 +1000

    Add dotfiles to magit-repository-directories

--- emacs/.doom.d/+magit.el
+++ emacs/.doom.d/+magit.el
@@ -7,7 +7,8 @@
   (magit-wip-after-save-mode t)
   (magit-wip-after-apply-mode t)
   (setq magit-save-repository-buffers 'dontask
-        magit-repository-directories '("/home/aria/projects/")
+        magit-repository-directories '(("/home/aria/projects/" . 3)
+                                       ("/home/aria/dotfiles/" . 0))
         magit-popup-display-buffer-action nil ;; Not sure why this is here, wonder what it does
         magit-display-file-buffer-function #'switch-to-buffer-other-window
         magithub-clone-default-directory "~/projects") ;; I want my stuff to clone to ~/projects
