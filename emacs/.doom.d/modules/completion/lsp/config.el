e8e0090edc50ca190b0dd84cc1d1c1af3a451437
Aria Edmonds <aria@ar1as.space>
2018-06-10 18:38:41 +1000

    Move set-face-attributes to a function

--- emacs/.doom.d/modules/completion/lsp/config.el
+++ emacs/.doom.d/modules/completion/lsp/config.el
@@ -10,25 +10,7 @@
   (set! :lookup 'lsp-ui-mode
     :definition #'lsp-ui-peek-find-definitions
     :references #'lsp-ui-peek-find-references)
-  ;; Stolen from https://github.com/syl20bnr/spacemacs/blob/develop/layers/%2Btools/lsp/funcs.el#L12
-  (set-face-attribute 'lsp-ui-peek-list nil
-                      :background (face-attribute 'hl-line :background nil t))
-  (set-face-attribute 'lsp-ui-peek-peek nil
-                      :background (face-attribute 'hl-line :background nil t))
-  (set-face-attribute 'lsp-ui-peek-selection nil
-                      :background (face-attribute 'highlight :background nil t)
-                      :foreground (face-attribute 'default :foreground nil t))
-  (set-face-attribute 'lsp-ui-peek-filename nil
-                      :foreground (face-attribute 'font-lock-constant-face
-                                                  :foreground nil t))
-  (set-face-attribute 'lsp-ui-peek-highlight nil
-                      :background (face-attribute 'highlight :background nil t)
-                      :foreground (face-attribute 'highlight :foreground nil t)
-                      :distant-foreground (face-attribute 'highlight
-                                                          :foreground nil t))
-  (set-face-attribute 'lsp-ui-peek-header nil
-                      :background (face-attribute 'highlight :background nil t)
-                      :foreground (face-attribute 'default :foreground nil t)))
+  (+peek-set-face-theme))
 
 (def-package! lsp-javascript-typescript
   :commands (lsp-javascript-typescript-enable)
@@ -68,10 +50,30 @@
 
 
 
-
 (when (featurep! +sh)
   (lsp-define-stdio-client lsp-sh
                            "sh"
                            #'(lambda () default-directory)
                            '("bash-language-server" "start"))
   (add-hook 'sh-mode-hook #'lsp-sh-enable))
+
+(defun +peek-set-face-theme ()
+  ;; Stolen from https://github.com/syl20bnr/spacemacs/blob/develop/layers/%2Btools/lsp/funcs.el#L12
+  (set-face-attribute 'lsp-ui-peek-list nil
+                      :background (face-attribute 'hl-line :background nil t))
+  (set-face-attribute 'lsp-ui-peek-peek nil
+                      :background (face-attribute 'hl-line :background nil t))
+  (set-face-attribute 'lsp-ui-peek-selection nil
+                      :background (face-attribute 'highlight :background nil t)
+                      :foreground (face-attribute 'default :foreground nil t))
+  (set-face-attribute 'lsp-ui-peek-filename nil
+                      :foreground (face-attribute 'font-lock-constant-face
+                                                  :foreground nil t))
+  (set-face-attribute 'lsp-ui-peek-highlight nil
+                      :background (face-attribute 'highlight :background nil t)
+                      :foreground (face-attribute 'highlight :foreground nil t)
+                      :distant-foreground (face-attribute 'highlight
+                                                          :foreground nil t))
+  (set-face-attribute 'lsp-ui-peek-header nil
+                      :background (face-attribute 'highlight :background nil t)
+                      :foreground (face-attribute 'default :foreground nil t)))
