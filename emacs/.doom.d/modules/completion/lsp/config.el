22ad56ded61b0c9da8c1f0e642c9f879df69c5af
Aria Edmonds <aria@ar1as.space>
2018-06-11 10:23:45 +1000

    Remove +peek-set-face-theme
    on default doom it looks like ass

--- emacs/.doom.d/modules/completion/lsp/config.el
+++ emacs/.doom.d/modules/completion/lsp/config.el
@@ -10,7 +10,6 @@
   (set! :lookup 'lsp-ui-mode
     :definition #'lsp-ui-peek-find-definitions
     :references #'lsp-ui-peek-find-references)
-  (+peek-set-face-theme)
   (setq lsp-ui-doc-max-height 8
         lsp-ui-doc-max-width 35
         lsp-ui-sideline-ignore-duplicate t))
@@ -85,23 +84,3 @@
                             '("bash-language-server" "start"))
     (add-hook 'sh-mode-hook #'lsp-sh-enable)))
 
-(defun +peek-set-face-theme ()
-  ;; Stolen from https://github.com/syl20bnr/spacemacs/blob/develop/layers/%2Btools/lsp/funcs.el#L12
-  (set-face-attribute 'lsp-ui-peek-list nil
-                      :background (face-attribute 'hl-line :background nil t))
-  (set-face-attribute 'lsp-ui-peek-peek nil
-                      :background (face-attribute 'hl-line :background nil t))
-  (set-face-attribute 'lsp-ui-peek-selection nil
-                      :background (face-attribute 'highlight :background nil t)
-                      :foreground (face-attribute 'default :foreground nil t))
-  (set-face-attribute 'lsp-ui-peek-filename nil
-                      :foreground (face-attribute 'font-lock-constant-face
-                                                  :foreground nil t))
-  (set-face-attribute 'lsp-ui-peek-highlight nil
-                      :background (face-attribute 'highlight :background nil t)
-                      :foreground (face-attribute 'highlight :foreground nil t)
-                      :distant-foreground (face-attribute 'highlight
-                                                          :foreground nil t))
-  (set-face-attribute 'lsp-ui-peek-header nil
-                      :background (face-attribute 'highlight :background nil t)
-                      :foreground (face-attribute 'default :foreground nil t)))
