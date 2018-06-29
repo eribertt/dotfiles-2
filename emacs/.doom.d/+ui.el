814f8b78bd4c6f91d830f7f490ffba97742a371e
Aria Edmonds <aria@ar1as.space>
2018-06-29 17:51:54 +1000

    Add pretty-symbols of haskell and clojure

--- emacs/.doom.d/+ui.el
+++ emacs/.doom.d/+ui.el
@@ -29,4 +29,6 @@
             ;; We need to trigger neotree-find then switch back to the buffer we just opened
             (save-current-buffer (neotree-find))))))
 
+(set-pretty-symbols! 'haskell-mode :iosevka t)
+(set-pretty-symbols! 'clojure-mode :iosevka t)
 (load! "+magit")
