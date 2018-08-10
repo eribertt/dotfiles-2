3ff1d19b05396c40393ae7b47adc057e86048388
Aria <aria@ar1as.space>
2018-08-10 14:52:35 +1000

    Iosevka -> Hack

--- emacs/.doom.d/+ui.el
+++ emacs/.doom.d/+ui.el
@@ -1,8 +1,8 @@
 ;;;  -*- lexical-binding: t; -*-
 
 ;; Fonts
-(setq doom-font (font-spec :family "Iosevka" :size 18))
-(setq doom-big-font (font-spec :family "Iosevka" :size 25))
+(setq doom-font (font-spec :family "Hack" :size 18))
+(setq doom-big-font (font-spec :family "Hack" :size 25))
 
 ;; Dash highlighting
 (after! dash (dash-enable-font-lock))
