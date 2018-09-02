bd88909278b5ce48e8eb918ad26993cb34bde921
Aria <aria@ar1as.space>
2018-09-02 21:57:19 +1000

    Add explanation and increase 25->28 doom-big-font

--- emacs/.doom.d/+ui.el
+++ emacs/.doom.d/+ui.el
@@ -2,7 +2,10 @@
 
 ;; Fonts
 (setq doom-font (font-spec :family "Hack" :size 18))
-(setq doom-big-font (font-spec :family "Hack" :size 25))
+;; On my (almost) 1440p monitor, 28pt Hack gives me about 30 lines and maybe
+;; 100-120 columns, which seems like a good zoomed in size for eye
+;; strain/showing other people stuff
+(setq doom-big-font (font-spec :family "Hack" :size 28))
 
 ;; Dash highlighting
 (after! dash (dash-enable-font-lock))
