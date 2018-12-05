032ec65f1bbfb2f8f9ebf2698be4804813a1bc5e
Aria <aria@ar1as.space>
2018-12-05 15:21:25 +1100

    Use ssh_url instead of clone_url for magithub

--- emacs/.doom.d/+magit.el
+++ emacs/.doom.d/+magit.el
@@ -16,4 +16,5 @@
                                        ("/home/aria/dotfiles/" . 0))
         magit-popup-display-buffer-action nil ;; Not sure why this is here, wonder what it does
         magit-display-file-buffer-function #'switch-to-buffer-other-window
-        magithub-clone-default-directory "~/projects")) ;; I want my stuff to clone to ~/projects
+        magithub-clone-default-directory "~/projects" ;; I want my stuff to clone to ~/projects
+        magithub-preferred-remote-method 'ssh_url)) ;; HTTPS cloning is awful, i authenticate with ssh keys.
