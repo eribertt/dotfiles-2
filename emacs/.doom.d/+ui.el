6d5ae49927cae358a15211ed92428079d21456e6
Aria <aria@ar1as.space>
2018-12-15 10:44:49 +1100

    use iosevka in emacs

--- emacs/.doom.d/+ui.el
+++ emacs/.doom.d/+ui.el
@@ -3,11 +3,11 @@
 (setq doom-theme 'doom-snazzy)
 
 ;; Fonts
-(setq doom-font (font-spec :family "Fantasque Sans Mono" :size 18))
+(setq doom-font (font-spec :family "Iosevka" :size 18))
 ;; On my (almost) 1440p monitor, 28pt Hack gives me about 30 lines and maybe
 ;; 100-120 columns, which seems like a good zoomed in size for eye
 ;; strain/showing other people stuff
-(setq doom-big-font (font-spec :family "Fantasque Sans Mono" :size 28))
+(setq doom-big-font (font-spec :family "Iosevka" :size 28))
 
 ;; Dash highlighting
 (after! dash (dash-enable-font-lock))
