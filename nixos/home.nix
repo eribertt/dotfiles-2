5837634780a9261169b39aedf688842ad153b24f
Aria Edmonds <aria@ar1as.space>
2019-01-21 03:21:01 +1100

    install xclip and gibo

--- nixos/home.nix
+++ nixos/home.nix
@@ -3,7 +3,7 @@
 {
   home.packages = with pkgs; [
     htop qutebrowser emacs termite networkmanagerapplet dunst libnotify mpv
-    pass rofi-pass discord gimp tldr bat pavucontrol spectacle
+    pass rofi-pass discord gimp tldr bat pavucontrol spectacle xclip gibo
 
     #betterlockscreen stuff
     i3lock-color imagemagick xorg.xdpyinfo xorg.xrandr bc feh
