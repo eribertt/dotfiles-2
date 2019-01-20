2e3d80f0f124d3980d2c89cbbce01241f1b5a946
Aria Edmonds <aria@ar1as.space>
2019-01-21 04:55:15 +1100

    install nmap

--- nixos/home.nix
+++ nixos/home.nix
@@ -4,7 +4,7 @@
   home.packages = with pkgs; [
     htop qutebrowser emacs termite networkmanagerapplet dunst libnotify mpv
     pass rofi-pass discord gimp tldr bat pavucontrol spectacle xclip gibo
-    spotify
+    spotify nmap
 
     #betterlockscreen stuff
     i3lock-color imagemagick xorg.xdpyinfo xorg.xrandr bc feh
