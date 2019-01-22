e2d25044aa66409ea4f0080d04e4de300abf452a
Aria Edmonds <aria@ar1as.space>
2019-01-22 16:09:03 +1100

    enable big font by default

--- emacs/.doom.d/+ui.el
+++ emacs/.doom.d/+ui.el
@@ -7,6 +7,7 @@
 ;; On my 1920x1200 monitor, 28pt Iosevka gives me about 33 lines and maybe 135
 ;; columns, which seems like a good zoomed in size for eye strain
 (setq doom-big-font (font-spec :family "Iosevka" :size 28))
+(add-hook! 'window-setup-hook #'doom-big-font-mode)
 ;; Dash highlighting
 (after! dash (dash-enable-font-lock))
 
